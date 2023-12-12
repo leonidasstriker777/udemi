8e177514a62a890c69696dab28bbeb62576fb96a
Leonardo Saucedo <leonardosaucedoservin@gmail.com>
2023-12-12 15:09:59 -0300

    VIEW. Cambiar primary key value por Url Unique

--- inmuebleslist_app/api/serializers.py
+++ inmuebleslist_app/api/serializers.py
@@ -10,13 +10,12 @@ class EdificacionSerializer(serializers.ModelSerializer):
         #exclude = ['id']
 
 
-class EmpresaSerializer(serializers.ModelSerializer):
+class EmpresaSerializer(serializers.HyperlinkedModelSerializer):
     
-    #edificacionlist = EdificacionSerializer(many=True, read_only=True)
+    edificacionlist = EdificacionSerializer(many=True, read_only=True)
     #edificacionlist = serializers.StringRelatedField(many=True)
     #edificacionlist = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    
-    edificacionlist = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='edificacion-detalle')
+    #edificacionlist = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='edificacion-detalle')
     
     class Meta:
         model = Empresa
--- inmuebleslist_app/api/views.py
+++ inmuebleslist_app/api/views.py
@@ -23,8 +23,41 @@ class EmpresaAV(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class EmpresaDetalleAV(APIView):
+    def get(self, request, pk):
+        try:
+            empresa = Empresa.objects.get(pk=pk)
+        except empresa.DoesNotExist:
+            return Response({'error': 'Empresa no encontrada'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = EmpresaSerializer(empresa, context={'request': request})
+        return Response(serializer.data)
+    
+    
+    def put(self, request, pk):
+        try:
+            empresa = Empresa.objects.get(pk=pk)
+        except empresa.DoesNotExist:
+            return Response({'error': 'Empresa no encontrada'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = EmpresaSerializer(empresa, data=request.data, context={'request': request})
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
         
         
+    def delete(self, request, pk):
+        try:
+            empresa = Empresa.objects.get(pk=pk)
+        except empresa.DoesNotExist:
+            return Response({'error': 'Empresa no encontrada'}, status=status.HTTP_404_NOT_FOUND)
+        
+        empresa.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class EdificacionAV(APIView):
