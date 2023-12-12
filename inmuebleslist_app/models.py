3b16bba61d65697c8a4baf72a817c6d41d7f67e7
Leonardo Saucedo <leonardosaucedoservin@gmail.com>
2023-12-12 13:37:33 -0300

    MODELS. Relacion entre modelos

--- inmuebleslist_app/models.py
+++ inmuebleslist_app/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=250)
+    website = models.URLField(max_length=250)
+    active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
 class Edificacion(models.Model):
     direccion = models.CharField(max_length=250)
     pais = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
     imagen = models.CharField(max_length=900)
     active = models.BooleanField(default=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='edificacion')
     created = models.DateTimeField(auto_now_add=True)
 
     
@@ -15,13 +25,6 @@ class Edificacion(models.Model):
     
     
 
-class Empresa(models.Model):
-    nombre = models.CharField(max_length=250)
-    website = models.URLField(max_length=250)
-    active = models.BooleanField(default=True)
-    
-    def __str__(self):
-        return self.nombre
     
     
     
