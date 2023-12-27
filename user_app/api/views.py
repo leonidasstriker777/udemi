a1fd2cef2e10bc25b6665c42ed94c7c409ead1b0
Leonardo Saucedo <leonardosaucedoservin@gmail.com>
2023-12-27 13:40:36 -0300

    43. Registro de nuevos usuarios y token de seguridad

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework.decorators import api_view
-from rest_framework.response   import Response
-from user_app.api.serializers  import RegistrationSerializer
-
+from rest_framework.decorators        import api_view
+from rest_framework.response          import Response
+from user_app.api.serializers         import RegistrationSerializer
+from rest_framework.authtoken.models  import Token
+from user_app                         import models
 
 # Registrar nuevo User
 @api_view(['POST'], )
@@ -9,10 +10,21 @@ def resgistration_view(request):
     if request.method == 'POST':
         # Obtiene datos del Cliente, en Json
         serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        
         # Valida el serializer Creado. Si es unico y comparacion de passwords
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            data['response'] = 'El registro del usuario fue exitoso'
+            data['username'] = account.username
+            data['email'] = account.email
+            # Retorna una instacia en Base64
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+        else:
+            data=serializer.errors
+            
+        return Response(data)
 
 
 
